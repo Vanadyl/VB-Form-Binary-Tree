9326d315c3698944c8920bd18c93a960714d7b21
Vanadyl <josephadderley@gmail.com>
2016-05-10 01:04:47 +0100

    Add files via upload

--- ConsoleApplication4/ConsoleApplication4/Module1.vb
+++ ConsoleApplication4/ConsoleApplication4/Module1.vb
@@ -1,7 +1,20 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Dim NodeTree As New Tree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.AddtoTree(Console.ReadLine)
+        NodeTree.inOrderTraverse(NodeTree._root)
+        NodeTree.postOrderTraverse(NodeTree._root)
+        NodeTree.preOrderTraverse(NodeTree._root)
+        Console.ReadLine()
     End Sub
 
 End Module
--- ConsoleApplication4/ConsoleApplication4/Tree.vb
+++ ConsoleApplication4/ConsoleApplication4/Tree.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Tree
-    Private _root As TreeNode
+    Public _root As TreeNode
     'Creating a new tree requires a root node, contained within this constructor
     Sub New(ByVal rootdata As Object)
         _root = GetNode(rootdata)
@@ -7,7 +7,7 @@
     Public Function GetNode(ByVal data As Object) As TreeNode
         Dim node As New TreeNode
         node.Data = data
-        Return
+        Return node
     End Function
 
     Public Sub AddtoTree(ByVal data As Object)
@@ -37,6 +37,20 @@
             inOrderTraverse(node.LeftNode)
             Console.WriteLine(node.Data)
             inOrderTraverse(node.RightNode)
+        End If
+    End Sub
+    Public Sub preOrderTraverse(ByVal node As TreeNode)
+        If Not node Is Nothing Then
+            Console.WriteLine(node.Data)
+            preOrderTraverse(node.LeftNode)
+            preOrderTraverse(node.RightNode)
+        End If
+    End Sub
+    Public Sub postOrderTraverse(ByVal node As TreeNode)
+        If node Is Nothing Then
+            postOrderTraverse(node.LeftNode)
+            Console.WriteLine(node.Data)
+            postOrderTraverse(node.RightNode)
         End If
     End Sub
 End Class
